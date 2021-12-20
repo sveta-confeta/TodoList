aa549210692cc29255219b1eadd8bfb87cbe264e
sveta <Sunkovasvetlana@gmail.com>
2021-12-20 17:01:46 +0300

    02lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TodoList} from "./TodoList";
 
-export type TaskType = {
-    id: number //для идентификации конкретной таски, когда их много
-    title: string
-    isDone: boolean
-}
+export type filterType = 'All' | 'Active' | 'Completed' //типизация фильтра для кнопок
 
 
 function App() {
     //BLL
-    const todoListTitle_1: string = 'What to Learn';
-    const todoListTitle_2: string = 'Songs';
+    const todoListTitle: string = 'What to Learn';
 
-    const tasks_1: Array<TaskType> = [  //для первого листа
-        {id: 1, title: 'НTML', isDone: true},
+    //hook
+    const [tasks, setTasks] = useState([ //чтоб происходила перерисовка видоизмененных данных
+        {id: 1, title: 'НTML', isDone: true},//use state принимает данные и возращает массив
         {id: 2, title: 'CSS', isDone: true},
         {id: 3, title: 'JS/TS', isDone: true},
-    ];
-    const tasks_2: Array<TaskType> = [  //для второго листа
-        {id: 4, title: 'Metallica', isDone: true},
-        {id: 5, title: 'Nirvana', isDone: true},
-        {id: 6, title: 'Green Day', isDone: true},
-    ];
+        {id: 4, title: 'CSS', isDone: false},
+        {id: 5, title: 'JS/TS', isDone: false},
+        {id: 6, title: 'CSS', isDone: true},
+        {id: 7, title: 'JS/TS', isDone: true},
+    ]);
+
+    // функция для кнопки удаления
+    const removeTask = (mId: number) => {  //mId-мы назвали id специально чтоб не путаться где чье id
+        setTasks(tasks.filter(f => f.id !== mId)) //функция удаления которая будет привязана к кнопке и ее надо через
+        //пропс поместить в туду лист
+    }
+    //hook
+    // const [filterValue, setFilterValue] = useState<filterType>('All') //all-по умолчанию
+    // console.log(filterValue);
+    //
+    //
+    // // фильтр для кнопок
+    // let isDoneTrue = tasks
+    // if (filterValue === 'Active') {
+    //     isDoneTrue = tasks.filter(f => f.isDone);
+    // }
+    // if (filterValue === 'Completed') {
+    //     isDoneTrue = tasks.filter(f => !f.isDone);
+    // }
+    // //принимает значение value от кнопок
+    // const filteredTasks = (value: filterType) => {  //принимаем от кнопки value (например'all')
+    //     setFilterValue(value);
+    // }
+    // const setFilter = (value:string) => {  //принимаем от кнопки value (например'all')
+    //   setMyFilter(value)                    //если 'all'-то дай все
+    //     console.log(filter);                  //если 'Active'-то дай не все
+    // }                                         //если 'completed'-то дай не не все
+
+
     return (
         <div className="App">
             <TodoList
-                title={todoListTitle_1}
-                tasks={tasks_1}
+                title={todoListTitle}
+                tasks={tasks}
+                removeTask={removeTask} //перебрасываем в тудулист функция удаления
+                // setFilter={setFilterValue} //передаем функцию и не забываем типизаровать в тудулисте
             />
-            <TodoList
-                title={todoListTitle_2}
-                tasks={tasks_2}
-            />
-
 
         </div>
     );
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,28 +1,61 @@
-import React from "react";
-import {TaskType} from "./App";
+import React, {useState} from "react";
+import s from './TodoList.module.css'
+import {filterType} from "./App";
+
+type TaskType = {
+    id: number //для идентификации конкретной таски, когда их много
+    title: string
+    isDone: boolean
+}
 
 type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
+     removeTask: (mId: number) => void//функция удаления
+    // setFilter: (value: filterType) => void //void-потому что функция ничего не возращает,без return
 }
 
 export function TodoList(props: TodoListPropsType) {
+
+    const [filterValue, setFilterValue] = useState<filterType>('All') //all-по умолчанию
+
+    // фильтр для кнопок
+    let isDoneTrue = props.tasks
+    if (filterValue === 'Active') {
+        isDoneTrue = isDoneTrue.filter(f => f.isDone);
+    }
+    if (filterValue === 'Completed') {
+        isDoneTrue= isDoneTrue.filter(f => !f.isDone);
+    }
+    //принимает значение value от кнопок
+    const filteredTasks = (value: filterType) => {  //принимаем от кнопки value (например'all')
+        setFilterValue(value);
+    }
+
+
     return (
         <div>
             <h3>{props.title}</h3>
-            <div>
+            <div>setTasks
                 <input/>
                 <button>+</button>
             </div>
-            <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+            <ul>{/* потому что в эту ul мы передаем array => tasks*/}
+                {isDoneTrue.map((m => {
+                    return (
+                        <li key={m.id}><input type="checkbox" checked={m.isDone}/> <span>{m.title}</span>
+                            <button onClick={() => props.removeTask(m.id)}
+                                    className={s.btn}>x
+                            </button>
+                        </li>
+                    )
+                }))}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => filteredTasks('All')}>All</button>
+                {/*через параметр мы передаем строку -идентификатор кнопки*/}
+                <button onClick={() => filteredTasks('Active')}>Active</button>
+                <button onClick={() => filteredTasks('Completed')}>Completed</button>
             </div>
         </div>
 
