77fe097bbb12a386f056069c4b06122c406184a9
sveta-confeta <_svet_@tut.by>
2022-01-28 09:26:05 +0300

    Updates

---
+++ precache-manifest.e5fe6df4f0b660f2ed33f172dd5ba70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e203ba7f5a906caa978d80ab9c7d6223",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "355261ddcdcf4f760a0b",
+    "url": "/TodoList/static/css/main.4501ac54.chunk.css"
+  },
+  {
+    "revision": "323b8a6b64a78a66a710",
+    "url": "/TodoList/static/js/2.5c4ff2aa.chunk.js"
+  },
+  {
+    "revision": "4662df181aea1747ef33698fb99e7915",
+    "url": "/TodoList/static/js/2.5c4ff2aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355261ddcdcf4f760a0b",
+    "url": "/TodoList/static/js/main.e42152c8.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
