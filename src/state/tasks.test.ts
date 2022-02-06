a3c9416dec2e2a4f024d75fdefbe788a12f7d886
sveta-confeta <_svet_@tut.by>
2022-02-06 22:52:57 +0300

    unit-test reducer Salary

--- src/state/tasks.test.ts
+++ src/state/tasks.test.ts
@@ -1,4 +1,4 @@
-import {addSalary} from "./tasks";
+import {addSalary, AddSalaryActionType, divSalary, fallSalary, multiplaySalary, salaryReducer} from "./tasks";
 
 test('addSalary',()=>{
     //1.test data
@@ -8,5 +8,53 @@ test('addSalary',()=>{
     const result=addSalary(salary,bonus);
     //3 проверка ожидаемого результата
     expect(result).toBe(950);
+    //звпуск теста например yarn test
+})
+test('fallSalary',()=>{
+    //1.test data
+    const salary:number=700;
+    const minus:number=250;
+    //2.Выполнение тестируемого кода action
+    const result=fallSalary(salary,minus);
+    //3 проверка ожидаемого результата
+    expect(result).toBe(450);
+    //звпуск теста например yarn test
+})
+test('multiplaySalary',()=>{
+    //1.test data
+    const salary:number=700;
+    const coefficient:number=2;
+    //2.Выполнение тестируемого кода action
+    const result=multiplaySalary(salary,coefficient);
+    //3 проверка ожидаемого результата
+    expect(result).toBe(1400);
+    //звпуск теста например yarn test
+})
+test('divSalary',()=>{
+    //1.test data
+    const salary:number=700;
+    const coefficient:number=2;
+    //2.Выполнение тестируемого кода action
+    const result=divSalary(salary,coefficient);
+    //3 проверка ожидаемого результата
+    expect(result).toBe(350);
+    //звпуск теста например yarn test
+})
+
+//test-reducer:
+
+test('case Add-SALARY of salaryReducer',()=>{
+    //1.test data
+    const salary:number=700;
+    const xxx: AddSalaryActionType={ //можно вместо ххх-action
+        type:'ADD-SALARY',
+        payload:{
+            bonus:300
+        }
+
+    }
+
+    //3 проверка ожидаемого результата
+    expect(salaryReducer(salary,xxx)).toBe(1000);
     //звпуск теста например yarn test
 })
--- src/state/tasks.ts
+++ src/state/tasks.ts
@@ -1,3 +1,35 @@
-export const addSalary=(salary:number,bonus:number)=>{
-    return salary+bonus
+export const addSalary = (salary: number, bonus: number) => {
+    return salary + bonus
+}
+export const fallSalary = (salary: number, minus: number) => {
+    return salary - minus;
+}
+export const multiplaySalary = (salary: number, coefficient: number) => {
+    return salary * coefficient;
+}
+export const divSalary = (salary: number, coefficient: number) => {
+    return salary / coefficient;
+}
+
+//создадим одну функцию для всех функций решающих одну задачу
+//1. в параметрах у них есть одна и та же сущность-salary. У нас это state.
+
+export type AddSalaryActionType = {
+    type: 'ADD-SALARY'
+    payload: {
+        bonus: number
+    }
+}
+
+
+type ActionType = AddSalaryActionType
+
+
+export const salaryReducer = (salary: number, action: ActionType) => {
+    switch (action.type) {
+        case 'ADD-SALARY':
+            return salary + action.payload.bonus;
+        default:
+            return salary;
+    }
 }
